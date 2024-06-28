37d841e49e519265dfb264f52b8d68805f1bb8ab
Joao Francisco Perpectuo Figueiredo <jperpect@c2r2s4.42porto.com>
2024-06-28 13:41:26 +0100

    ja com  norama falta tirar o que esta a mais

--- _client/client.c
+++ _client/client.c
@@ -1,80 +1,72 @@
-
-#include <signal.h>
-#include <fcntl.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jperpect <jperpect@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/28 13:36:16 by jperpect          #+#    #+#             */
+/*   Updated: 2024/06/28 13:38:03 by jperpect         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "../libft/libft/libft.h"
 #include "../libft/printf/ft_printf.h"
-
+#include <fcntl.h>
+#include <signal.h>
 
 void	handlers(int sig, siginfo_t *info, void *ucontext)
 {
-	// if(sig == SIGUSR1 )
-	// 	write(1,"ola\n",4);
-	// else
-	// 	write(1,"ola\n",4);
-  
+	if (sig == SIGUSR2)
+		ft_printf("fim de mensagem\n");
 }
 
-static void server()
+static void	server(void)
 {
- struct sigaction sa;
- static int a ;
- a = 0;
+	struct sigaction	sa;
+	static int			a;
 
- sigemptyset(&sa.sa_mask);
-
- sa.sa_sigaction = handlers;
- sa.sa_flags = SA_RESTART;
-
-  //ft_printf("PID e = %d \n",getpid());
-
- sigaction(SIGUSR1,&sa,NULL);
- sigaction(SIGUSR2,&sa,NULL);
-
- //pause();
+	a = 0;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_sigaction = handlers;
+	sa.sa_flags = SA_RESTART;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
 }
 
-static void env_bit(char *str,pid_t ok)
+static void	env_bit(char *str, pid_t ok)
 {
-  int i;
-  i = -1;
-  
-  while(str[++i] != '\0')
-  {
-    if(str[i] == '0')
-      kill(ok,SIGUSR1);
-    if (str[i]== '1')
-      kill(ok,SIGUSR2);
-    usleep(4000);
-    //pause();
-  }
+	int	i;
 
+	i = -1;
+	while (str[++i] != '\0')
+	{
+		if (str[i] == '0')
+			kill(ok, SIGUSR1);
+		if (str[i] == '1')
+			kill(ok, SIGUSR2);
+		usleep(4000);
+	}
 }
 
-int main(int ac ,char **av )
+int	main(int ac, char **av)
 {
+	int		i;
+	char	b[9];
+	pid_t	server_pid;
 
-    server();
-  if ( ac == 3)
-  {
-    int i;
-    char b[9];
-    i= -1;
-    pid_t server_pid = ft_atoi(av[1]);
-    while(av[2][++i] != '\0')
-    {
-      ft_bzero(b,9);
-      ft_str_btis(8,av[2][i],b);
-      
-      //ft_printf("bits - %s\n", b);
-      env_bit(b,server_pid);
-    }
-    ft_bzero(b,9);
-   
-    ft_printf("bits - %s\n", "00000000");
-    env_bit("00000000",server_pid);
-    
-
-  }
-  
+	server();
+	if (ac == 3)
+	{
+		i = -1;
+		server_pid = ft_atoi(av[1]);
+		while (av[2][++i] != '\0')
+		{
+			ft_bzero(b, 9);
+			ft_str_btis(8, av[2][i], b);
+			env_bit(b, server_pid);
+		}
+		ft_bzero(b, 9);
+		env_bit("00000000", server_pid);
+	}
 }
--- _server/server.c
+++ _server/server.c
@@ -6,163 +6,146 @@
 /*   By: jperpect <jperpect@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/07 21:31:45 by jperpect          #+#    #+#             */
-/*   Updated: 2024/06/27 17:02:16 by jperpect         ###   ########.fr       */
+/*   Updated: 2024/06/28 13:35:29 by jperpect         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
-#include <stdio.h>
-#include <signal.h>
-#include <unistd.h>
-#include <fcntl.h>
-
 #include "../libft/libft/libft.h"
 #include "../libft/printf/ft_printf.h"
+#include <fcntl.h>
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
 
-
-void concat(char *str,char *bits)
+void	concat(char *str, char *bits)
 {
-	int a = 9;
-	int zero;
+	int	a;
+	int	zero;
+	int	i;
+	int	len;
+
+	a = 9;
 	zero = 0;
-	int i = 0;
-	while(--a >= 0)
+	i = 0;
+	while (--a >= 0)
 	{
-		if(bits[i] == '1')
-	 		zero = (zero<<1) | 1 ;
-		else if(bits[i] == '0')
-			zero= zero<<1;
+		if (bits[i] == '1')
+			zero = (zero << 1) | 1;
+		else if (bits[i] == '0')
+			zero = zero << 1;
 		i++;
 	}
-	int len = ft_strlen(str);
+	len = ft_strlen(str);
 	str[len] = zero;
-	str[len+1] = '\0';
+	str[len + 1] = '\0';
 }
 
-int print_and_clear(t_list *save_list,char *save,int *len,int *index )
+int	print_and_clear(t_list *save_list, char *save, int *len, int *index)
 {
-	ft_lstptint(save_list,1);
-			ft_putstr_fd(save,1);
-			ft_lstclear(&save_list);
-			len[0] = 0;
-			index[0] = 0;
-			ft_bzero(save,11);
-			return(0);
-}
-
-
-
-t_list *list_control(int *len,int *index,char *save)
-{
-	t_list *element;
-	static t_list *list;
-	static t_list *save_list ;
+	ft_lstptint(save_list, 1);
+	ft_putstr_fd(save, 1);
+	ft_lstclear(&save_list);
 	len[0] = 0;
-			usleep(50);
-			if (index[0] == 0)
-			{
-				list = ft_lstnew(ft_strdup(save));;
-				save_list = list;		
-			}
-			else
-			{
-			element = ft_lstnew(ft_strdup(save));		
-			ft_lstadd_back(&list,element);		
-			list = list->next;
-			}
-			ft_bzero(save,11);
-			index[0]++;
-			return(save_list);
+	index[0] = 0;
+	ft_bzero(save, 11);
+	return (0);
 }
 
+t_list	*list_control(int *len, int *index, char *save)
+{
+	t_list			*element;
+	static t_list	*list;
+	static t_list	*save_list;
+
+	len[0] = 0;
+	usleep(50);
+	if (index[0] == 0)
+	{
+		list = ft_lstnew(ft_strdup(save));
+		save_list = list;
+	}
+	else
+	{
+		element = ft_lstnew(ft_strdup(save));
+		ft_lstadd_back(&list, element);
+		list = list->next;
+	}
+	ft_bzero(save, 11);
+	index[0]++;
+	return (save_list);
+}
 
 void	handlers(int sig, siginfo_t *info, void *ucontext)
 {
-	static  int ids = -1;
-	static char letter[8] = "22222222";
-	static  int len = 0;
-	static  int index = 0;
-	static t_list *save_list ;
-	static char save[11];
-	
-	if(sig == SIGUSR1 )
-		letter[++ids] = '0'; 	
-	else if ( sig == SIGUSR2)
+	static int		ids = -1;
+	static char		letter[8] = "22222222";
+	static int		len[2];
+	static t_list	*save_list;
+	static char		save[11];
+
+	if (sig == SIGUSR1)
+		letter[++ids] = '0';
+	else if (sig == SIGUSR2)
 		letter[++ids] = '1';
 	if (ids == 7)
 	{
-		concat(save,letter);
-		if(ft_strncmp("00000000",letter,8) == 0)
-			 ids = print_and_clear(save_list,save,&len,&index );
-		ft_bzero(letter,8);
-		if (len == 10)
-			save_list = list_control(&len,&index,save);
+		concat(save, letter);
+		if (ft_strncmp("00000000", letter, 8) == 0)
+		{
+			ids = print_and_clear(save_list, save, &len[0], &len[1]);
+			kill(info->si_pid, SIGUSR2);
+		}
+		ft_bzero(letter, 8);
+		if (len[0] == 10)
+			save_list = list_control(&len[0], &len[1], save);
 		ids = -1;
-		len++;
+		len[0]++;
 	}
-	kill(info->si_pid,SIGUSR1);
+	kill(info->si_pid, SIGUSR1);
 }
 
-
-// int main()
-// {
-// 	struct sigaction sa;
-	
-
-// 	sigemptyset(&sa.sa_mask);
-// 	sa.sa_sigaction = handlers;
-// 	sa.sa_flags = SA_RESTART | SA_SIGINFO;
-	
-
-
-
-// 	printf("PID e = %d \n",getpid());
-	
-	
-// 	ft_putnbr_fd(getpid(),1);	
-// 	sigaction(SIGUSR1,&sa,NULL);
-// 	sigaction(SIGUSR2,&sa,NULL);
-
-// 	while(1)
-// 	{
-// 		pause();
-// 	}
-
-
-// }
-
-int main()
+int	main(void)
 {
-
-	remove("number.txt");
-	
-	struct sigaction sa;
+	struct sigaction	sa;
 
 	sigemptyset(&sa.sa_mask);
 	sa.sa_sigaction = handlers;
 	sa.sa_flags = SA_RESTART | SA_SIGINFO;
-	
-	
-
-	int fd = open("number.txt", O_WRONLY | O_CREAT, 0644 );
-	
-	ft_putnbr_fd(getpid(),fd);	
-	ft_printf("💥 len %d",ft_strlen("💥"));
-	char *a = "💥";	
-	ft_putstr_fd(a,1);
-	close(fd);
-	
-	
-	ft_printf("PID e = %d \n",getpid());
-	
-	sigaction(SIGUSR1,&sa,NULL);
-	sigaction(SIGUSR2,&sa,NULL);
-	
-
-	while(1)
+	ft_printf("PID e = %d \n", getpid());
+	ft_putnbr_fd(getpid(), 1);
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	while (1)
 	{
 		pause();
 	}
-
-
 }
+
+// int	main(void)
+// {
+// 	remove("number.txt");
+
+// 	struct sigaction sa;
+
+// 	sigemptyset(&sa.sa_mask);
+// 	sa.sa_sigaction = handlers;
+// 	sa.sa_flags = SA_RESTART | SA_SIGINFO;
+
+// 	int fd = open("number.txt", O_WRONLY | O_CREAT, 0644);
+
+// 	ft_putnbr_fd(getpid(), fd);
+// 	ft_printf("💥 len %d", ft_strlen("💥"));
+// 	char *a = "💥";
+// 	ft_putstr_fd(a, 1);
+// 	close(fd);
+
+// 	ft_printf("PID e = %d \n", getpid());
+
+// 	sigaction(SIGUSR1, &sa, NULL);
+// 	sigaction(SIGUSR2, &sa, NULL);
+
+// 	while (1)
+// 	{
+// 		pause();
+// 	}
+// }
